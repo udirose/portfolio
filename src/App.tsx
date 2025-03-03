0701eb9da45219ad2123f8374e1cd1635d69cc82
udirose <udirose@wustl.edu>
2025-03-03 17:22:14 -0600

    add custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Projects from './components/projects/Projects';
 import Contact from './components/Contact/Contact';
 import withIntersectionObserver from './intersectionObserver';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const AboutWithObserver = withIntersectionObserver(About);
 const ProjectsWithObserver = withIntersectionObserver(Projects);
@@ -11,16 +12,18 @@ const ContactWithObserver = withIntersectionObserver(Contact);
 
 const App: React.FC = () => {
     return (
-        <div>
-            <header>
-                <h1 className="wave-animation">Hi, I am Udi Rose <span className="wave-emoji">👋</span></h1>
-            </header>
-            <main>
-                <AboutWithObserver />
-                <ProjectsWithObserver />
-                <ContactWithObserver />
-            </main>
-        </div>
+        <Router basename="/">
+            <div>
+                <header>
+                    <h1 className="wave-animation">Hi, I am Udi Rose <span className="wave-emoji">👋</span></h1>
+                </header>
+                <main>
+                    <AboutWithObserver />
+                    <ProjectsWithObserver />
+                    <ContactWithObserver />
+                </main>
+            </div>
+        </Router>
     );
 }
 
